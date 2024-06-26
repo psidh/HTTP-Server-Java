4bcb8088d51a2ca13fab2f45f65381bd14d22919
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-06-26 16:19:54 +0530

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,20 +6,15 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Main {
+public class Main{
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
-    //
+
     try {
       serverSocket = new ServerSocket(4221);
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
       clientSocket = serverSocket.accept(); // Wait for connection from client.
       
@@ -32,16 +27,17 @@ public class Main {
       System.out.println("request: " + HTTPRequest[1]);
       OutputStream out  = clientSocket.getOutputStream();
 
-      System.out.println("console: " + HTTPRequest[1].substring(0, HTTPRequest.length - 1));
+      // Correctly use the request path
+      String requestPath = HTTPRequest[1];
       
-      if (HTTPRequest[1].substring(0, HTTPRequest.length - 1).equals("/echo/banana")){
-        out.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nbanana".getBytes());
-      } else if(HTTPRequest[1].substring(0, HTTPRequest.length - 1).equals("/")) {
-        out.write("HTTP/1.1 200 Not Found\r\n\r\n".getBytes());
-      } else{
+      if (requestPath.equals("/echo/banana")) {
+        out.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 6\r\n\r\nbanana".getBytes());
+      } else if(requestPath.equals("/")) {
+        out.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n".getBytes());
+      } else {
         out.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
       }
-    
+
       out.flush();
       System.out.println("accepted new connection");
 
@@ -50,5 +46,3 @@ public class Main {
     }
   }
 }
-
-
